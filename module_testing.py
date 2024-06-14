2467174a1aad36ffe6cd70b94ca5e21e764fca24
Goodchild21 <Goodchild_21@mail.ru>
2024-06-15 00:27:48 +0300

    Add files via upload

--- module_testing.py
+++ module_testing.py
@@ -1,6 +1,39 @@
 import unittest
+from solution import Positions, Programmer
 
-# Place your tests here
+
+class TestProgrammer(unittest.TestCase):
+
+    def test_init(self):
+
+        programmer = Programmer("Alexey", Positions.JUNIOR)
+        self.assertEqual(programmer._Programmer__name, "Alexey")
+        self.assertEqual(programmer._Programmer__position, Positions.JUNIOR)
+        self.assertEqual(programmer._Programmer__hour_price, Positions.JUNIOR.value)
+
+    def test_work(self):
+        programmer = Programmer("Alexey", Positions.JUNIOR)
+        programmer.work(150)
+        self.assertEqual(programmer._Programmer__times, 150)
+
+    def test_rise(self):
+        programmer = Programmer("Alexey", Positions.JUNIOR)
+        programmer.rise()
+        self.assertEqual(programmer._Programmer__position, Positions.MIDDLE)
+        self.assertEqual(programmer._Programmer__hour_price, Positions.MIDDLE.value)
+
+        programmer.rise()
+        self.assertEqual(programmer._Programmer__position, Positions.SENIOR)
+        self.assertEqual(programmer._Programmer__hour_price, Positions.SENIOR.value)
+
+        programmer.rise()
+        self.assertEqual(programmer._Programmer__hour_price, Positions.SENIOR.value + 2)
+
+    def test_info(self):
+        programmer = Programmer("Alexey", Positions.JUNIOR)
+        programmer.work(150)
+        self.assertEqual(programmer.info(), "Alexey: 150 ч. 1500 тгр.")
+
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
--- solution.py
+++ solution.py
@@ -1,32 +1,62 @@
-class Positions(Enum):  
-    JUNIOR = 10  
+from enum import Enum #Импорт класса Enum для работы с перечислениями из модуля enum
+
+class Positions(Enum):
+    JUNIOR = 10 #name=JUNIOR, value=10  
     MIDDLE = 12  
     SENIOR = 20  
-  
-  
-class Programmer:  
-    def __init__(self, name: float, position: Positions) -> None:  
+
+
+class Programmer: 
+    def __init__(self, name: str, position: Positions) -> None:  
         self.__name = name  
-        self.__position = position  
-        self.__hour_price = self.__position.value  
+        self.__position = position
+        self.__hour_price = self.__position.value
+
+
+    def work(self, time: int) -> None:
+        self.__times = 0 #Перенес инициализацию времени для срабатывания в методе work и для сброса в ноль для нового расчета (типо месяца)
+        self.__times += time
   
-    def work(self, time: int) -> None:  
-        self.__time += time  
-  
+
     def rise(self) -> str:  
         if self.__position.name == 'JUNIOR':  
-            self.__position = Positions.MIDDLE  
+            self.__position = Positions.MIDDLE
+            self.__hour_price = Positions.MIDDLE.value #Добавил расчет новой ставки
   
         elif self.__position.name == 'MIDDLE':  
-            self.__position = Positions.SENIOR  
+            self.__position = Positions.SENIOR
+            self.__hour_price = Positions.SENIOR.value
   
         else:  
             self.__hour_price += 2 
   
+
     def info(self) -> str:  
-        return f'{self.__name}: {self.__time} ч. {self.__give_salary()} тгр.'  
+        return f'{self.__name}: {self.__times} ч. {self.__give_salary()} тгр.'  
+
   
     def __give_salary(self) -> int:  
-        salary = self.__hour_price // self.__time  
-        self.__time = 0  
+        salary = self.__hour_price * self.__times #Заменил расчет ЗП ставка умножить на время
         return salary
+
+
+
+#Сбил все обращения под функцию
+def main():
+    batashev = Programmer(name='Alexey', position=Positions.JUNIOR)#Закидываем в Programmer Position с атрибутами Джуна
+    batashev.work(150)
+    print(batashev.info())#ЗП Джуна
+
+    batashev.rise()
+    batashev.work(150)
+    print(batashev.info())#ЗП Мидла
+
+    batashev.rise()
+    batashev.work(150)
+    print(batashev.info())#ЗП Сеньера
+
+    batashev.rise()
+    batashev.work(150)
+    print(batashev.info())#ЗП Сеньера+
+if __name__ == '__main__':
+    main()
